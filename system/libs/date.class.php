9fd87a457c920e46f1bbc337236433e82b392e94
Adam Shannon <adam@ashannon.us>
2010-11-22 15:13:12 -0600

    add in some libraries

---
+++ system/libs/date.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * BPA 2010
+ * Adam Shannon
+ * 2010-03-21
+ */
+
+class Date {
+	
+	static public $format = 'H:i:s \o\n Y-m-d';
+	
+	
+	/**
+	 * parse(int timestamp)
+	 * This function will parse a unix timestamp and return the 
+	 * value in self::$format's format.
+	 *
+	 * @parm: int timestamp - The UNIX timestamp
+	 * @return mixed - The date in self::$format's format.
+	 */
+	static function parse($timestamp) {
+		
+		return @date(self::$format, $timestamp);
+		
+	}
+}
