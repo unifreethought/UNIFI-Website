376b00034816178f6b8c3464143f623fc7e9c177
Adam Shannon <adam@ashannon.us>
2010-11-22 19:34:49 -0600

    i18n update

---
+++ application/i18n/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * UNIFI Website
+ * Adam Shannon
+ * 2010-11-21
+ */
+ 
+define('LANGUAGE_ID', 'English');
+define('BASE_URL', 'http://unifreethought.com');
+define('SITE_TITLE', 'UNI Freethinkers and Inquirers');
+
+define('WEB_ACCESS_DISABLED', 'We\'re sorry, but this website is down.');
