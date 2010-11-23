4c57a68223dd340451e69b6b7689533f73120650
Adam Shannon <adam@ashannon.us>
2010-11-22 19:32:10 -0600

    basic structure for a user class

---
+++ application/helpers/user.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * UNIFI Website
+ * Adam Shannon
+ * 2010-11-21
+ */
+ 
+class User {
+	
+	static function auth($user, $pass) {
+		
+	}
+	
+	static function get($cookie) {
+		
+	}	
+	
+}
