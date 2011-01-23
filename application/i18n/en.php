9d2bb33bcf37232136e1d5cb8744cde01290264e
Adam Shannon <adam@ashannon.us>
2011-01-22 18:21:34 -0600

    Create a dashbord link for editing blog posts

--- application/i18n/en.php
+++ application/i18n/en.php
@@ -73,6 +73,7 @@ define('PAGE_LIST_EVENTS_TITLE', 'List Events');
 define('ADMIN_DASHBORD_TITLE', 'Admin Dashbord');
 define('ADMIN_DASHBORD_LIST_NAME', 'Items');
 define('ADMIN_DASHBORD_POST_BLOG', 'Post a Blog Post');
+define('ADMIN_DASHBORD_EDIT_BLOG_POSTS', 'Edit Blog Posts');
 define('ADMIN_DASHBORD_LIST_MEMBERS', 'List Members');
 define('ADMIN_DASHBORD_EDIT_MEMBER', 'Edit Member(Feature requires a user to edit)');
 define('ADMIN_DASHBORD_SEARCH_USERS', 'Search Users');
@@ -80,4 +81,3 @@ define('ADMIN_DASHBORD_CREATE_EVENT', 'Create Event');
 define('ADMIN_DASHBORD_LIST_EVENTS', 'List Events');
 define('ADMIN_DASHBORD_EDIT_EVENT', 'Edit Event (Feature requires an event to edit)');
 define('ADMIN_DASHBORD_EDIT_EVENT_ATTENDANCE', 'Edit Event Attendance (Feature requires an event to edit)');
-			
