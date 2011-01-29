c996cbf47b4685310a73cf8c104ab24686490e2c
Adam Shannon <adam@ashannon.us>
2011-01-29 14:55:54 -0600

    There should be another link to 'list blog posts'

--- application/i18n/en.php
+++ application/i18n/en.php
@@ -72,13 +72,19 @@ define('PAGE_LIST_EVENTS_TITLE', 'List Events');
 // Dashbord Titles
 define('ADMIN_DASHBORD_TITLE', 'Admin Dashbord');
 define('ADMIN_DASHBORD_LIST_NAME', 'Items');
+
 define('ADMIN_DASHBORD_POST_BLOG', 'Post a Blog Post');
+define('ADMIN_DASHBORD_LIST_BLOG_POSTS', 'List Blog Posts');
+	define('ADMIN_DASHBORD_LIST_BLOG_POSTS_DESC', ' (list all of the posts)');
 define('ADMIN_DASHBORD_EDIT_BLOG_POSTS', 'Edit Blog Posts');
 	define('ADMIN_DASHBORD_EDIT_BLOG_POSTS_DESC', ' (content changes, draft/live changing, deleting, etc..)');
+	
 define('ADMIN_DASHBORD_LIST_MEMBERS', 'List Members');
 define('ADMIN_DASHBORD_EDIT_MEMBER', 'Edit Member');
 	define('ADMIN_DASHBORD_EDIT_MEMBER_DESC', '(Feature requires a user to edit)');
+
 define('ADMIN_DASHBORD_SEARCH_USERS', 'Search Users');
+
 define('ADMIN_DASHBORD_CREATE_EVENT', 'Create Event');
 define('ADMIN_DASHBORD_LIST_EVENTS', 'List Events');
 define('ADMIN_DASHBORD_EDIT_EVENT', 'Edit Event');
