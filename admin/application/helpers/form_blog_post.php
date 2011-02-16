30dcb5539a4a97155867e0ba3ad48d89f41c43fb
Adam Shannon <adam@ashannon.us>
2011-02-16 17:10:29 -0600

    Edit the admin blog poster to allow some html. <strong><b><em><i><img><a> This still needs to be tested for security purposesgit add ./

--- admin/application/helpers/form_blog_post.php
+++ admin/application/helpers/form_blog_post.php
@@ -6,7 +6,8 @@
 
 $time = MySQL::clean(@time());
 $title = MySQL::clean($_POST['title']);
-$text = MySQL::clean(htmlentities($_POST['text']));
+//$text = MySQL::clean(htmlentities($_POST['text']));
+$text = MySQL::clean(strip_tags($_POST['text'], $config['allowable_blog_tags']));
 
 $sql = "INSERT INTO `{$database}`.`blog-posts` (`id`,`author`, `timestamp`, `title`, `content`) VALUES ";
 $sql .= "('0', '{$user_id}', '{$time}', '{$title}', '{$text}');";
--- admin/application/helpers/form_blog_post_edit.php
+++ admin/application/helpers/form_blog_post_edit.php
@@ -7,7 +7,8 @@
  
 $id = MySQL::clean(htmlentities((int) $_POST['blog_id']));
 $title = MySQL::clean(htmlentities($_POST['title']));
-$content = MySQL::clean(htmlentities($_POST['content']));
+// $content = MySQL::clean(htmlentities($_POST['content']));
+$content = MySQL::clean(strip_tags($_POST['content'], $config['allowable_blog_tags']));
 
 $sql = "UPDATE `{$database}`.`blog-posts` SET `title` = '{$title}', `content` =  '{$content}' WHERE  `blog-posts`.`id` = '{$id}';";
 MySQL::query($sql);
