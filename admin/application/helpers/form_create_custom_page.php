a7b1f2bd38a80c6ae2916f5e7f6b84155f97edab
Adam Shannon <adam@ashannon.us>
2011-03-04 19:13:31 -0600

    Log the creation of creating custom pages.

--- admin/application/helpers/form_create_custom_page.php
+++ admin/application/helpers/form_create_custom_page.php
@@ -25,4 +25,7 @@ $sql = "INSERT INTO `{$database}`.`pages` (`id`,`url_stub`,`page_title`,`content
 $sql .= "('0','{$url_stub}','{$name}','Edit Me!!');";
 MySQL::query($sql);
 
+// Log the creation
+Log::create($user_id, 'create_custom_page', "name:{$name}<br>url:{$url_stub}");
+
 header('Location: index.php');
