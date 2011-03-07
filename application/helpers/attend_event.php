f90da361be586b0bacaf5d71e7750069606ddbf0
Adam Shannon <adam@ashannon.us>
2011-03-07 01:54:30 -0600

    Fix a few bugs with attending events...

--- application/helpers/attend_event.php
+++ application/helpers/attend_event.php
@@ -8,9 +8,15 @@
 $event_id = MySQL::clean($_GET['event_id']);
 $user_id = MySQL::clean($user_id);
 
-$sql = "UPDATE `{$database}`.`event_notifications` SET `rsvp` = '1' WHERE `event_notifications`.`id` = '{$event_id}' AND ";
+$sql = "UPDATE `{$database}`.`event_notifications` SET `rsvp` = '1' WHERE `event_notifications`.`event_id` = '{$event_id}' AND ";
 $sql .= "`event_notifications`.`user_id` = '{$user_id}';";
 
+//echo $sql . '<br />';
 MySQL::query($sql);
+
+$sqla = "INSERT INTO `{$database}`.`event_attendance` (`event_id`,`user_id`) VALUES ('{$event_id}','{$user_id}');";
+
+//echo $sqla;
+MySQL::query($sqla);
 
 header('Location: index.php?page=view_events');
