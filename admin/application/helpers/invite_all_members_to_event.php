95aeedc0e2f6b29bfd3db7550c5dc2e16723e51b
Adam Shannon <adam@ashannon.us>
2011-03-07 02:32:22 -0600

    Fix a bug where old events were still counted in the Events(N) nav item.

--- admin/application/helpers/invite_all_members_to_event.php
+++ admin/application/helpers/invite_all_members_to_event.php
@@ -7,10 +7,11 @@
  
 $event_id = MySQL::clean($_GET['event_id']);
 $user_ids = MySQL::search("SELECT `id` FROM `{$database}`.`users`");
+$timestamp = MySQL::single("SELECT `end_time` FROM `{$database}`.`events` WHERE `id` = '{$event_id}' LIMIT 1");
 
 foreach ($user_ids as $id) {
 	$sql = "INSERT INTO `{$database}`.`event_notifications` (`id`,`user_id`,`event_id`,`timestamp`,`rsvp`) VALUES ";
-	$sql .= "('0', '{$id['id']}', '{$event_id}', '1934567890', '0')";
+	$sql .= "('0', '{$id['id']}', '{$event_id}', '{$timestamp['end_time']}', '0')";
 	MySQL::query($sql);
 }
 
