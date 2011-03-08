0abb99c8b69128fab4923f7e1622017e9174ba71
Adam Shannon <adam@ashannon.us>
2011-03-08 02:05:34 -0600

    Fix the Events(N) to only be displayed when a user is logged in.

--- application/helpers/print_events_link.php
+++ application/helpers/print_events_link.php
@@ -6,13 +6,14 @@
  */
  
 function print_events_link($user_id, $database) {
+
+	if (isset($user_id)) {	
+		$user_id = MySQL::clean($user_id);
+		$now = MySQL::clean(@time());
 	
-	$user_id = MySQL::clean($user_id);
-	$now = MySQL::clean(@time());
+		$sql = "SELECT COUNT(*) FROM `{$database}`.`event_notifications` WHERE `user_id` = '{$user_id}' AND `timestamp` > '{$now}' AND `rsvp` = '0'";
 	
-	$sql = "SELECT COUNT(*) FROM `{$database}`.`event_notifications` WHERE `user_id` = '{$user_id}' AND `timestamp` > '{$now}' AND `rsvp` = '0'";
-	
-	$event_count = MySQL::single($sql);
-	echo "<a class='nav_item' href='index.php?page=view_events'>" . MAIN_PAGE_EVENTS_NAV_LINK . ' (' . $event_count['COUNT(*)'] . ")</a>";
-	
+		$event_count = MySQL::single($sql);
+		echo "<a class='nav_item' href='index.php?page=view_events'>" . MAIN_PAGE_EVENTS_NAV_LINK . ' (' . $event_count['COUNT(*)'] . ")</a>";
+	}	
 }
