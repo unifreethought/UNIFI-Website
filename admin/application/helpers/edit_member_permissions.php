139edb3e89190b532a8c9b3c8f254694371bc4ae
Adam Shannon <adam@ashannon.us>
2011-03-14 21:47:40 -0500

    Create the front end for editing user permissions.

--- admin/application/helpers/edit_member_permissions.php
+++ admin/application/helpers/edit_member_permissions.php
@@ -4,5 +4,23 @@
  * Adam Shannon
  * 2011-03-14
  */
- 
 
+function show_checked($permission) {
+	if ($permission == '1') {
+		echo ' checked';
+	}
+}
+
+if (empty($_GET['user'])) {
+	include 'templates/' . $config['template'] . '/html/edit_member_permissions_get_user.html';
+} else {
+	$user = MySQL::clean($_GET['user']);
+	
+	$sql = "SELECT `first_name`,`last_name` FROM `{$database}`.`users` WHERE `id` = '{$user}' LIMIT 1;";
+	$data = MySQL::single($sql);
+	
+	$sql = "SELECT * FROM `{$database}`.`user-permissions` WHERE `user_id` = '{$user}' LIMIT 1";
+	$permissions = MySQL::single($sql);
+
+	include 'templates/' . $config['template'] . '/html/edit_member_permissions.html';
+}
