c529c0b96b0c1eef2561eea788f3240040f6f321
Adam Shannon <adam@ashannon.us>
2011-04-16 17:39:05 -0500

    Forgot to update a class on the profile link.

--- application/helpers/print_profile_button.php
+++ application/helpers/print_profile_button.php
@@ -8,7 +8,7 @@
 function print_profile_button($user_id) {
 	
 	if (isset($user_id)) {
-		echo "<a class='nav_item' href='index.php?page=profile&profile={$user_id}'>" . MAIN_PAGE_PROFILE_NAV_LINK . "</a>";
+		echo "<a class='profile-link' href='index.php?page=profile&profile={$user_id}'>" . MAIN_PAGE_PROFILE_NAV_LINK . "</a>";
 	}
 	
 }
