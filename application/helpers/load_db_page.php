4f97986a0480b8d9e8faab86b724fac95e1feee4
Adam Shannon <adam@ashannon.us>
2011-05-19 15:40:34 -0500

    Fix db pages.

--- application/helpers/load_db_page.php
+++ application/helpers/load_db_page.php
@@ -5,6 +5,8 @@
  * 2010-11-21
  */
  
+include 'show_authors_and_labels.php';
+ 
 // Load the contents for a page from the db, then display 
 // it in a general template (all data loaded should be global).
 $db_page_data = MySQL::single("SELECT * FROM `" . MySQL::$database . "`.`pages` WHERE `url_stub` = '" . MySQL::clean($url['page']) . "' LIMIT 1");
