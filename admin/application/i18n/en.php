2e893bfc843bc870a9f8a4d7b8273d88bae6ffdd
Adam Shannon <adam@ashannon.us>
2011-07-06 21:05:37 -0500

    Move i18n to the admin side also...

--- admin/application/i18n/en.php
+++ admin/application/i18n/en.php
@@ -92,3 +92,126 @@ define('ADMIN_DASHBOR_EDIT_MEMBER_PERMISSIONS_GET_USER_DESC', 'Editing permissio
 	define('ADMIN_DASHBOR_EDIT_MEMBER_PERMISSIONS_EDIT_EVENT_ATTENDANCE', 'Edit Event Attendance');
 	define('ADMIN_DASHBOR_EDIT_MEMBER_PERMISSIONS_EDIT_CUSTOM_PAGES', 'Edit Custom Pages');
 	define('ADMIN_DASHBOR_EDIT_MEMBER_PERMISSIONS_VIEW_LOG', 'View Log');
+	
+// Common Errors
+define('NO_USER_ID', 'Whoops, you need to specify a user.');
+define('NO_FB_ID_HELP', 'Login with Facebook: ');
+
+// Facebook dialogs
+define('FB_LOGIN_OUT_ALERT', 'Press OK to reload the page and finish loggin in/out.');
+
+// Footer links
+define('FOOTER_LEGAL', 'Legal & Privacy');
+define('FOOTER_CONTACT', 'Contact');
+define('FOOTER_TOP', 'Top');
+
+// Profile Nav Link
+define('MAIN_PAGE_PROFILE_NAV_LINK', 'Profile');
+define('PROFILE_HEADER_SUFFIX', '\'s Profile');
+define('PROFILE_DOES_NOT_EXIST', 'We\'re sorry, but this profile doesn\'t seem to exist.');
+
+// Legal
+define('LEGAL_HEADER', 'UNIFI Website Usage and Privacy Policy');
+
+// Events Nav Link
+define('MAIN_PAGE_EVENTS_NAV_LINK', 'Events');
+define('EVENTS_HEADER', 'Your Upcoming Events');
+define('EVENTS_TABLE_TITLE_TITLE', 'Title');
+define('EVENTS_TABLE_TITLE_START_TIME', 'Start Time');
+define('EVENTS_TABLE_TITLE_END_TIME', 'End Time');
+define('EVENTS_TABLE_TITLE_LOCATION', 'Location');
+define('EVENTS_TABLE_TITLE_DESCRIPTION', 'Description');
+define('EVENTS_TABLE_TITLE_RSVP', 'RSVP');
+define('EVENTS_BUTTON_ATTEND', 'Attend');
+
+// Registration
+define('REGISTER_HEADER', 'Please Register');
+define('REGISTER_SUBMIT', 'Register');
+define('REGISTER_MESSAGE', 'It seems like you\'re not registred here.  All it takes is to fill out this form, and then you will just sign in with Facebook from now on.');
+define('REGISTER_FB_ID', 'Facebook ID: ');
+define('REGISTER_FIRST_NAME', 'First Name: ');
+define('REGISTER_LAST_NAME', 'Last Name: ');
+define('REGISTER_GENDER', 'Gender: ');
+define('REGISTER_FEMALE', 'Female');
+define('REGISTER_MALE', 'Male');
+define('REGISTER_TRANSGENDER', 'Transgender');
+
+// Blog
+define('BLOG_POST_NOT_THERE', 'We\'re sorry, but it looks like this post doesn\'t exist.');
+define('BLOG_AUTHOR_PREFIX', 'By ');
+define('BLOG_OLDER_POSTS', 'Older posts');
+define('BLOG_COMMENT_REPLY', 'Leave a reply');
+define('BLOG_COMMENT_SUBMIT', 'Submit');
+define('BLOG_COMMENT_FAIL', 'Sorry, but you must be logged in to comment.');
+define('BLOG_POST_COMMENTS', 'Comments');
+
+// Listing blog posts
+define('LIST_BLOG_ID', 'ID #1');
+define('LIST_BLOG_AUTHOR', 'Author');
+define('LIST_BLOG_TIMESTAMP', 'Date');
+define('LIST_BLOG_TITLE', 'Title');
+define('LIST_BLOG_CONTENT', 'Content');
+
+// Edit blog posts
+define('EDIT_BLOG_POST_HEADER', 'Edit a blog post.');
+define('EDIT_BLOG_POST_TITLE', 'Title: ');
+define('EDIT_BLOG_POST_SUBMIT', 'Edit Post');
+
+// List Events
+define('LIST_EVENT_ID', 'ID #');
+define('LIST_EVENT_TITLE', 'Title');
+define('LIST_EVENT_START_TIME', 'Start Time');
+define('LIST_EVENT_END_TIME', 'End Time');
+define('LIST_EVENT_LOCATION', 'Location');
+define('LIST_EVENT_DESCRIPTION', 'Description');
+
+// Editing events
+define('EDIT_EVENT_TITLE', 'Title: ');
+define('EDIT_EVENT_LOCATION', 'Location: ');
+define('EDIT_EVENT_START', 'Start: ');
+define('EDIT_EVENT_END', 'End: ');
+define('EDIT_EVENT_ATTENDING', 'Members Attending');
+define('EDIT_EVENT_SUBMIT', 'Edit Event');
+define('EDIT_EVENT_INVITE_MEMBERS', 'Invite Members');
+
+// Listing of Members
+define('LIST_MEMBERS_HEADER', 'User Listing');
+define('LIST_MEMBERS_id', 'id');
+define('LIST_MEMBERS_First', 'First');
+define('LIST_MEMBERS_Last', 'Last');
+define('LIST_MEMBERS_Year', 'Year');
+define('LIST_MEMBERS_Major', 'Major');
+define('LIST_MEMBERS_Dorm', 'Dorm');
+define('LIST_MEMBERS_Phone', 'Phone');
+define('LIST_MEMBERS_Email', 'Email');
+define('LIST_MEMBERS_Texting', 'Texting');
+define('LIST_MEMBERS_Positions', 'Position(s)');
+define('LIST_MEMBERS_Tags', 'Tag(s)');
+define('LIST_MEMBERS_Notes', 'Notes');
+define('LIST_MEMBERS_Recruit_Place', 'Recruit Place');
+define('LIST_MEMBERS_Hometown', 'Hometown');
+
+// Creating Members
+define('CREATE_MEMBER_FACEBOOK', 'Facebook');
+define('CREATE_MEMBER_FACEBOOK_ID', 'Get Facebook ID');
+define('CREATE_MEMBER_FACEBOOK_ID_BUTTON', 'Loading...');
+
+// Searching members
+define('FORM_NO_VALUE', 'None');
+define('MEMBER_SEARCH', 'Member Search');
+define('MEMBER_SEARCH_DESC', '<strong>Searching members</strong>: Enter the value(s) that you want to use to limit the search.');
+
+// Page Titles
+define('PAGE_CREATE_USER_HEADER', 'Create User');
+define('PAGE_CREATE_USER_DESC', '(Desc)');
+define('PAGE_EDIT_USER_HEADER', 'Edit User');
+define('PAGE_EDIT_USER_DESC', 'Below are the values for a user, feel free to edit them as needed and the site will handle the data. Thanks :)');
+define('PAGE_CREATE_EVENT_HEADER', 'Create Event');
+define('PAGE_POST_TO_BLOG', 'Create a blog post');
+define('PAGE_POST_TO_BLOG_TITLE_FIELD', 'Title: ');
+define('PAGE_POST_TO_BLOG_ALLOWED_TAGS', 'Allowed Tags: ');
+define('PAGE_POST_TO_BLOG_ADD_TAGS', 'Update Tags');
+define('PAGE_POST_TO_BLOG_CREATE_TAG', 'Create Tag');
+define('PAGE_LIST_EVENTS_TITLE', 'List Events');
+define('PAGE_LIST_BLOG_POSTS', 'Our Blog Posts');
+define('PAGE_EDIT_EVENT_HEADER', 'Edit an Event');
