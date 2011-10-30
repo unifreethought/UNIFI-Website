982bbd8e49261e961fb4979a0d7a45e6b9e204c8
Adam Shannon <adam@ashannon.us>
2011-10-29 23:37:09 -0500

    Edit functionality to edit membership attendance.

--- admin/application/helpers/edit_event.php
+++ admin/application/helpers/edit_event.php
@@ -8,22 +8,9 @@
 include 'system/libs/show_date.php';
  
 // Pull the data for the event
-$event_id = MySQL::clean($_GET['event_id']);
+$event_id = MySQL::clean($_GET['event']);
 $event = MySQL::single("SELECT * FROM `{$database}`.`events` WHERE `id` = '{$event_id}' LIMIT 1");
-	
-// Pull the people who went to the event.
-$attendance = MySQL::search("SELECT `user_id` FROM `{$database}`.`event_attendance` WHERE `event_id` = '{$event_id}';");
 
-// Pull the first and last names for the people who attendended.
-$members = array();
-foreach ($attendance as $item) {
-	// We need to grab the first and last names for the specific user.
-	$tmp = MySQL::single("SELECT `first_name`,`last_name` FROM `{$database}`.`users` WHERE `id` = '{$item['user_id']}' LIMIT 1");
-	$members[] = array(
-		'user_id' => $item['user_id'],
-		'name' => $tmp['first_name'] . ' ' . $tmp['last_name']
-	);
-}
-
-$start = Show_Date::parse($event['start_time']);
-$end = Show_Date::parse($event['end_time']);
+// Load them into vars
+$event['start'] = Show_Date::parse($event['start_time']);
+$event['end'] = Show_Date::parse($event['end_time']);
