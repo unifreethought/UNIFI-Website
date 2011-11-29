ca234f1e2a88696a65f942e5457e5531baea3b50
Adam Shannon <adam@ashannon.us>
2011-11-28 20:43:41 -0600

    Include a sample config file.

---
+++ application/config/config.sample.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * UNIFI Website
+ * Adam Shannon
+ * 2010-11-21
+ */
+
+// How do you want to handle errors?
+error_reporting(E_NONE);
+
+// Database Info
+$hostname = '';
+$username = '';
+$password = '';
+$database = '';
+
+// The Facebook App info
+define('FACEBOOK_APP_ID', '');
+define('FACEBOOK_SECRET', '');
+
+// Connect
+MySQL::set_vars($hostname, $username, $password, $database);
+
+// Set some settings
+$config = array(
+	'admin' => 'enabled',
+	'web' => 'enabled',
+	'i18n' => 'en.php',
+	'i18n-admin' => 'en.php',
+	'allowable_blog_tags' => '<strong><b><em><i><img><a><iframe>',
+	'template' => 'epoch', // Don't include the trailing slash
+	'admin_email' => ''
+);
