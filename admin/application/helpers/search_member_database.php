809543e4bc83b92cfca77a3db14bc1339a178cdb
Adam Shannon <adam@ashannon.us>
2012-01-17 22:52:04 -0600

    Make searching the member_db have very loose contraints.

--- admin/application/helpers/search_member_database.php
+++ admin/application/helpers/search_member_database.php
@@ -7,18 +7,18 @@
 include 'system/libs/user.php';
 User_Parse::set($database);
 
-$first_name 	= ($_GET['first_name'] == '') ? '' : " `first_name` = '" . MySQL::clean($_GET['first_name']) . "' AND ";
-$last_name 		= ($_GET['last_name'] == '') ? '' : " `last_name` = '" . MySQL::clean($_GET['last_name']) . "' AND ";
+$first_name 	= ($_GET['first_name'] == '') ? '' : " `first_name` LIKE '%" . MySQL::clean($_GET['first_name']) . "%' AND ";
+$last_name 		= ($_GET['last_name'] == '') ? '' : " `last_name` LIKE '%" . MySQL::clean($_GET['last_name']) . "%' AND ";
 
 $year 			= ($_GET['year'] == '') ? '' : " `year` = '" . MySQL::clean($_GET['year']) . "' AND ";
 $major 			= ($_GET['major'] == '') ? '' : " `major` = '" . MySQL::clean($_GET['major']) . "' AND ";
 $dorm 			= ($_GET['dorm'] == '') ? '' : " `dorm` = '" . MySQL::clean($_GET['dorm']) . "' AND ";
 $recruit_place 	= ($_GET['recruit_place'] == '') ? '' : " `recruit_place` = '" . MySQL::clean($_GET['recruit_place']) . "' AND ";
-$phone 			= ($_GET['phone'] == '') ? '' : " `phone` = '" . MySQL::clean($_GET['phone']) . "' AND ";
+$phone 			= ($_GET['phone'] == '') ? '' : " `phone` LIKE '%" . MySQL::clean($_GET['phone']) . "%' AND ";
 $texting 		= ($_GET['texting'] == '') ? '' : " `texting` = '" . MySQL::clean($_GET['texting']) . "' AND ";
-$hometown 		= ($_GET['hometown'] == '') ? '' : " `hometown` = '" . MySQL::clean($_GET['hometown']) . "' AND ";
-$address 		= ($_GET['address'] == '') ? '' : " `address` = '" . MySQL::clean($_GET['address']) . "' AND ";
-$email 			= ($_GET['email'] == '') ? '' : " `email` = '" . MySQL::clean($_GET['email']) . "' AND ";
+$hometown 		= ($_GET['hometown'] == '') ? '' : " `hometown` LIKE '%" . MySQL::clean($_GET['hometown']) . "%' AND ";
+$address 		= ($_GET['address'] == '') ? '' : " `address` LIKE '%" . MySQL::clean($_GET['address']) . "%' AND ";
+$email 			= ($_GET['email'] == '') ? '' : " `email` LIKE '%" . MySQL::clean($_GET['email']) . "%' AND ";
 
 $positions_bad 	= explode(',', $_GET['positions']);
 $tags_bad		= explode(',', $_GET['tags']);
