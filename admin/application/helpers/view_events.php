7382dc13cf80cfd20132f78873701e2a79b30950
Adam Shannon <adam@ashannon.us>
2012-02-12 14:31:51 -0600

    Show all events from the past six weeks, instead of two.

--- admin/application/helpers/view_events.php
+++ admin/application/helpers/view_events.php
@@ -4,6 +4,7 @@
  * Adam Shannon
  */
 
-$time = @time() - (60 * 60 * 24 * 7 * 2);
+// Past six weeks
+$time = @time() - (60 * 60 * 24 * 7 * 6);
 $events = MySQL::search("SELECT * FROM `{$database}`.`events` WHERE `start_time` > {$time};");
 
