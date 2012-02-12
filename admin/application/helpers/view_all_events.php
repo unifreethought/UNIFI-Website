c90b08fe4b31e7f5959b60ef31d20af15f8025c1
Adam Shannon <adam@ashannon.us>
2012-02-12 14:53:45 -0600

    I might want to refine the results to an actual year.

--- admin/application/helpers/view_all_events.php
+++ admin/application/helpers/view_all_events.php
@@ -9,6 +9,7 @@ if (empty($year)) {
   $year = @date('Y');
 }
 $year_timestamp = @date(mktime(0, 0, 0, 0, 0, $year));
+$year_plus_one = @date(mktime(0, 0, 0, 0, 0, $year+1));
 
-$events = MySQL::search("SELECT * FROM `{$database}`.`events` WHERE `start_time` > {$year_timestamp}");
+$events = MySQL::search("SELECT * FROM `{$database}`.`events` WHERE `start_time` > '{$year_timestamp}' AND `start_time` < '{$year_plus_one}'");
 
