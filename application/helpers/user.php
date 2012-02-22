bd91d634815f5cec055c08c55aee6daaaa27275c
Adam Shannon <adam@ashannon.us>
2012-02-22 05:45:36 -0600

    Fix the issue where logging in was not shown on the page until the next page reload.

--- application/helpers/user.php
+++ application/helpers/user.php
@@ -15,14 +15,8 @@ function print_fb_script() {
     <script>
       window.fbAsyncInit = function() {
        	  FB.init({appId: ' . FACEBOOK_APP_ID . ', status: true, cookie: true, xfbml: true});
-		  FB.Event.subscribe(\'auth.sessionChange\', function(response) {
-			if (response.session) {
-			  // A user has logged in, and a new cookie has been saved
+		  FB.Event.subscribe(\'auth.authResponseChange\', function(response) {
 			  window.location.reload();
-			} else {
-			  // The user has logged out, and the cookie has been cleared
-			  window.location.reload();
-			}
 		  });
       };
       (function() {
