f187dc5127b99a35eb9df43a8e305cd3d18d2daf
Adam Shannon <adam@ashannon.us>
2012-02-22 08:27:23 -0600

    Link directly to the comments if they click that link on the mobile version.

--- mobile/application/i18n/en.php
+++ mobile/application/i18n/en.php
@@ -5,6 +5,7 @@
  */
 
 // Blog posts
-define('BLOG_AUTHOR_PREFIX', "By ");
-define('BLOG_POST_COMMENTS', "View Comments");
+define('BLOG_AUTHOR_PREFIX', 'By ');
+define('BLOG_POST_COMMENTS', 'View Comments');
 define('BLOG_POST_NOT_THERE', 'Sorry, this blog post doesn\'t seem to exist.');
+define('BLOG_COMMENTS_HEADER', 'Comments: ');
