cfbe58fb1430126f27f5e7a0246ee6ee2bf6dc79
Adam Shannon <adam@ashannon.us>
2012-03-11 16:28:28 -0500

    Fix a hole where any profile could be viewed by a guest.

--- application/helpers/show_profile.php
+++ application/helpers/show_profile.php
@@ -17,10 +17,9 @@ User_Parse::set($database);
 $id = MySQL::clean($_GET['profile']);
 
 $generic_data = MySQL::single("SELECT `first_name`,`last_name`,`facebook` FROM `{$database}`.`users` WHERE `id` = '{$id}' LIMIT 1");
-//print_r($generic_data);
 
 // Check to see if the user actually exists
-if (empty($generic_data['first_name'])) {
+if (empty($generic_data['first_name']) || empty($user_id)) {
 	include 'templates/' . $config['template'] . '/html/show_no_profile.html';
 	exit();
 }
@@ -28,5 +27,4 @@ if (empty($generic_data['first_name'])) {
 $full_data = MySQL::single("SELECT * FROM `{$database}`.`user-data` WHERE `id` = '{$id}' LIMIT 1");
 
 $full_data['phone'] = '(' . substr($full_data['phone'], 0, 3) . ') ' . substr($full_data['phone'], 3, 3) . '-' . substr($full_data['phone'], 5, 4);
-//print_r($full_data);
 
