91a20c707a3a776fe75d6cf43a13490c15f8467d
Adam Shannon <adam@ashannon.us>
2012-04-01 11:50:22 -0500

    Make twitter async

--- system/libs/social_media.class.php
+++ system/libs/social_media.class.php
@@ -30,7 +30,7 @@ class SocialMedia {
   }
 
   static function twitterButton($postId) {
-    echo "<a href='https://twitter.com/share' class='twitter-share-button' data-url='{self::buildUrl($postId)' data-via='unifreethought'>Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>";
+    echo "<a href='https://twitter.com/share' class='twitter-share-button' data-url='{self::buildUrl($postId)' data-via='unifreethought'>Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.async=true;js.src='//platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>";
   }
 
 }
