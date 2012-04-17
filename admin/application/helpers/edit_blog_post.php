0f345ea78a37cf354fa1170a5336646f725c690f
Adam Shannon <adam@ashannon.us>
2012-04-16 23:49:40 -0500

    Fixing a sql error that happens often.

--- admin/application/helpers/edit_blog_post.php
+++ admin/application/helpers/edit_blog_post.php
@@ -5,8 +5,10 @@
  * 2010-11-21
  */
 
+$postId = (empty($_GET['blog_post'])) ? "-1" : $_GET['blog_post'];
+
 // Pull the data for a post.
-$data = MySQL::single("SELECT `title`,`content` FROM `{$database}`.`blog-posts` WHERE `id` = " . MySQL::clean($_GET['blog_post']));
+$data = MySQL::single("SELECT `title`,`content` FROM `{$database}`.`blog-posts` WHERE `id` = " . MySQL::clean($postId));
 $data['id'] = MySQL::clean($_GET['blog_post']);
 
 $tags = MySQL::search("SELECT `id`,`tag` FROM `{$database}`.`blog-tag-names`");
