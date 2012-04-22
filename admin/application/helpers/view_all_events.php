7f0078b33542f5a56c85eaa90f650fa745bbd833
Adam Shannon <adam@ashannon.us>
2012-04-22 12:37:06 -0500

    Add a column to the pages that list events showing how many people attended.

--- admin/application/helpers/view_all_events.php
+++ admin/application/helpers/view_all_events.php
@@ -12,3 +12,11 @@ $year_timestamp = @date(mktime(0, 0, 0, 0, 0, $year));
 $year_plus_one = @date(mktime(0, 0, 0, 0, 0, $year+1));
 
 $events = MySQL::search("SELECT * FROM `{$database}`.`events` WHERE `start_time` > '{$year_timestamp}' AND `start_time` < '{$year_plus_one}'");
+
+$attendance = array();
+foreach ($events as $event) {
+  $sql = "SELECT COUNT(*) FROM `{$database}`.`event_attendance` WHERE `event_id` = '{$event['id']}';";
+  $result = MySQL::single($sql);
+  $attendance[$event['id']] = $result['COUNT(*)'];
+}
+
--- admin/application/helpers/view_events.php
+++ admin/application/helpers/view_events.php
@@ -7,3 +7,11 @@
 // Past six weeks
 $time = @time() - (60 * 60 * 24 * 7 * 6);
 $events = MySQL::search("SELECT * FROM `{$database}`.`events` WHERE `start_time` > {$time};");
+
+$attendance = array();
+foreach ($events as $event) {
+  $sql = "SELECT COUNT(*) FROM `{$database}`.`event_attendance` WHERE `event_id` = '{$event['id']}';";
+  $result = MySQL::single($sql);
+  $attendance[$event['id']] = $result['COUNT(*)'];
+}
+
