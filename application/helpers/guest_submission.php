c6881bcdcba8ad28f6c63da89778eb7494804489
Adam Shannon <adam@ashannon.us>
2012-05-05 12:15:43 -0500

    Guest submissions should email out to the web team.

--- application/helpers/guest_submission.php
+++ application/helpers/guest_submission.php
@@ -19,5 +19,8 @@ $sql = "INSERT INTO `{$database}`.`blog-guest-submissions` (`id`,`author`,`times
 $sql .= "('0', '{$author}', '{$timestamp}', '{$title}', '{$content}');";
 MySQL::query($sql);
 
+$headers = "From: contact@unifreethought.com";
+mail("webmaster@unifreethought.com", "A new guest submission.", "From: {$author}\n\nContent:\n{$content}", $headers);
+
 header('Location: index.php');
 exit();
