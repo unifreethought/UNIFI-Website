10c46564f0f89c4357cf078b6e8652bacb8e98ff
Adam Shannon <adam@ashannon.us>
2012-05-10 20:58:25 -0500

    Fix blog posts and drafts.

--- admin/application/helpers/form_blog_post_edit.php
+++ admin/application/helpers/form_blog_post_edit.php
@@ -7,12 +7,28 @@
 
 $id = MySQL::clean(htmlentities((int) $_POST['blog_id']));
 $title = MySQL::clean(htmlentities($_POST['title']));
-$content = MySQL::clean($_POST['content']);
+$content = MySQL::clean(htmlentities($_POST['content']));
 $action = MySQL::clean($_POST['publish-post']);
 if (empty($action)) {
   $table = 'blog-drafts';
 } else {
   $table = 'blog-posts';
+}
+
+function send_email() {
+  Log::create($user_id, 'new_blog_post', 'date:' . Date::parse($time) . '<br>title:' . $title);
+
+  // Set the headers for the email
+  $headers  = "From: UNI Freethinkers and Inquirers <contact@unifreethought.com>\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+  // Email all of the people setup for it.
+  $emails_raw = MySQL::single("SELECT `emails` from `{$database}`.`email_lists` WHERE `desc` = 'posting_emails' LIMIT 1");
+  $emails = explode(',', $emails_raw['emails']);
+  foreach ($emails as $email) {
+    mail($email, "A new blog post: " . $title, "{$content}", $headers);
+  }
 }
 
 if (empty($id)) {
@@ -41,25 +57,20 @@ if (empty($id)) {
     $sql = "INSERT INTO `{$database}`.`blog-guids` (`post_id`, `guid`) VALUES ('{$new_post_id['id']}', UUID());";
     MySQL::query($sql);
 
-    Log::create($user_id, 'new_blog_post', 'date:' . Date::parse($time) . '<br>title:' . $title);
-
-    // Set the headers for the email
-    $headers  = "From: UNI Freethinkers and Inquirers <contact@unifreethought.com>\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    // Email all of the people setup for it.
-    $emails_raw = MySQL::single("SELECT `emails` from `{$database}`.`email_lists` WHERE `desc` = 'posting_emails' LIMIT 1");
-    $emails = explode(',', $emails_raw['emails']);
-    foreach ($emails as $email) {
-      mail($email, "A new blog post: " . $title, "{$content}", $headers);
-    }
+    send_email();
   }
 
 } else {
-  $sql = "UPDATE `{$database}`.`{$table}` SET `title` = '{$title}', `content` =  '{$content}' WHERE  `{$table}`.`id` = '{$id}';";
+  $action = MySQL::clean($_POST['publish-post']);
+
+  if ($action == 'Submit') {
+    $sql = "INSERT INTO `{$database}`.`blog-posts` (`id`,`author`, `timestamp`, `title`, `content`) VALUES ";
+    $sql .= "('0', '{$user_id}', " .time() . ", '{$title}', '{$content}');";
+    send_email();
+  } else {
+    $sql = "UPDATE `{$database}`.`{$table}` SET `title` = '{$title}', `content` =  '{$content}' WHERE  `{$table}`.`id` = '{$id}';";
+  }
   MySQL::query($sql);
 }
 
 header('Location: index.php?page=list_blog_' . substr($table, 5));
-
