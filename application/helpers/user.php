bb46acf6f161b289ce409d966089d79697316b45
Adam Shannon <adam@ashannon.us>
2012-06-27 19:40:54 -0500

    Fixing the javascript code to not refresh the page but to add an element into the dom.

--- application/helpers/user.php
+++ application/helpers/user.php
@@ -15,7 +15,11 @@ function print_fb_script() {
       window.fbAsyncInit = function() {
            FB.init({appId: ' . FACEBOOK_APP_ID . ', status: true, cookie: true, xfbml: true});
       FB.Event.subscribe(\'auth.authResponseChange\', function(response) {
-        window.location.reload();
+        var html = "<h3>Admin</h3><div><div class=\'no-border\'><span><a href=\'admin/index.php\'>Dashboard</a></span></div></div>";
+        var adminBox = document.createElement(\'select\');
+            adminBox.innerHTML = html;
+            adminBox.id = \'admin-box\';
+        document.querySelector(\'#connect-with-us\').insertBefore(adminBox);
       });
       };
       (function() {
